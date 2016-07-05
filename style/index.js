604a4504c78dde2ecc591798f965ee6519332daa
繁复 <mentalitylc@foxmail.com>
2016-07-05 10:32:04 +0800

    首页布局
    简单一个首页布局

---
+++ style/index.js
@@ -0,0 +1,11 @@
+window.onload=function(){
+	var mask=document.getElementById("mask");
+	var width=window.innerWidth;
+	var height=window.innerHeight;
+	
+	
+	mask.onclick=function(){
+		mask.style.display="none";
+	}
+	
+}
